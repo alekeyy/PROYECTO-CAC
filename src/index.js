4626da87bb2f568da4556b705274047b35c5456b
shamelart <shamelart.ca@gmail.com>
2024-05-17 10:16:19 -0300

    Añadiendo localStorage a Contacto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-let arrItems = [];
+window.addEventListener("load", (e) => {
+  e.preventDefault();
+  let arrItems = [];
 
-function renderItem(arrACopiar){
-    const arrNuevo = arrACopiar.map(x=>item(x));
+  function renderItem(arrACopiar) {
+    const arrNuevo = arrACopiar.map((x) => item(x));
     console.log("asdasd");
 
     document.getElementById("catalogo").innerHTML = arrNuevo.join(" ");
-}
+  }
 
-function item(x){
+  function item(x) {
     return `
     <div class="item">
         <img src="./img/shop/${x.id}.webp" alt="Caja de saquitos de té">
@@ -16,21 +18,49 @@ function item(x){
         <a href="" class="hero--button">COMPRAR</a>
     </div>
     `;
-}
+  }
 
-function main(){
+  function main() {
     // console.log(TeaData);
 
-    fetch('src/apiData.json')
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            for(let teaItem of data.TeaData){ //teaItem debido a que item es una funcion
-                arrItems.push(teaItem)
-            }
-            console.log(arrItems);
-            renderItem(arrItems); //Lo movi aca por que si esta fuera del then, se ejecuta primero sin que suceda la insercion de datos.
-        })
-}
+    fetch("src/apiData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        for (let teaItem of data.TeaData) {
+          //teaItem debido a que item es una funcion
+          arrItems.push(teaItem);
+        }
+        console.log(arrItems);
+        renderItem(arrItems); //Lo movi aca por que si esta fuera del then, se ejecuta primero sin que suceda la insercion de datos.
+      });
+  }
 
-main();
+  main();
+
+  // @**** Función guarga la información suministrada por el usuario en el localStorage ****
+  const formulario = document.getElementById("form-storage");
+
+  formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nombre = formulario.nombre.value;
+    const email = formulario.email.value;
+    const telefono = formulario.telefono.value;
+    const comentario = formulario.comentario.value;
+
+    // Crea un objeto para almacenar los valores con sus claves
+    let storage = {
+      nombre,
+      email,
+      telefono,
+      comentario,
+    };
+
+    // Guarda el objeto en el localStorage
+    localStorage.setItem("storage", JSON.stringify(storage));
+
+    // Limpia los campos de entrada después de guardar la información
+    formulario.reset();
+  });
+});
